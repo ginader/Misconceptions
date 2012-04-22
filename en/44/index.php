1b3fb1eabbb13c2103bcf40042e146f23a71a312
Cindy Li <cindyli@251.sub-75-208-242.myvzw.com>
2012-04-22 11:57:14 -0700

    adding in 44, 48 questions

--- en/44/index.php
+++ en/44/index.php
@@ -1,7 +1,7 @@
 <?php include '../header.inc.php'; ?>
 <div class="face" id="question">
 	<h2><?php echo $question ?></h2>
-	<p>The Sun goes around the Earth in less than a year.</p>
+	<p>The Sun is never directly overhead at noon.</p>
 	<div class="cta">
 		<button id="yes"><?php echo $yes ?></button>
 		<button id="no"><?php echo $no ?></button>
@@ -12,10 +12,10 @@
 	<p>
 		<strong class="yes">Wrong.</strong>
 		<strong class="no">Correct!</strong> 
-		The earth goes around the sun in 365.24 days. Most of the time our calendar is 365 days and every 4th year it is 366 days. That's why we have a leap year!
+		For continental U.S. the answer is never. Since the Earth's rotation axis is tilted 23.5 degrees with respect to its orbital motion around the Sun, one would have to be less than 23.5 degrees above or below the equator to have the Sun pass directly overhead (once per year).
 	</p>
 	<div class="cta">
-		<a target="_blank" class="button" href="http://www.astronomy.org/astronomy/misconceptions.html#3" id="more">I want to learn more about the earth and sun!</a>
+		<a target="_blank" class="button" href="http://scienceblogs.com/dotphysics/2009/01/when-is-the-sun-directly-overhead.php" id="more">I want to learn more about sun!</a>
 		<a class="button" href="<?php echo $link_to_next ?>" id="next"><?php echo $next ?></a>
 	<div>
 </div>
