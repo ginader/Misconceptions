ea812a7048d0cf26c1e36b0ca0b38ca15ef51556
Cindy Li <cindyli@251.sub-75-208-242.myvzw.com>
2012-04-22 16:51:56 -0700

    shortening the answers

--- en/15/index.php
+++ en/15/index.php
@@ -12,8 +12,8 @@
 	<h2><?php echo $answer ?></h2>
 	<img src="/assets/img/img-solar-system-2.png" alt="Our solar system with all the planets.">
 	<p>
-		<strong class="yes">Wrong.</strong>
-		<strong class="no">Correct!</strong> 
+		<strong class="yes">Correct!</strong>
+		<strong class="no">Wrong.</strong> 
 		The Earth rotates on an axis, making one full rotation every 24 hours. Different sides of the Earth are exposed to the sun during the course of these 24 hours, with half of the planet being exposed to sun while the other half is deprived of light. It is this rotation that causes day and night.
 	</p>
 	<div class="cta">
