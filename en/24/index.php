fdafbb804f2a3614b1700738b59f3c91e61cee27
Cindy Li <cindyli@251.sub-75-208-242.myvzw.com>
2012-04-22 11:03:37 -0700

    adding in planets for cards

--- en/24/index.php
+++ en/24/index.php
@@ -1,7 +1,7 @@
 <?php include '../header.inc.php'; ?>
 <div class="face" id="question">
 	<h2><?php echo $question ?></h2>
-	<p>The Earth is larger than the Sun.</p>
+	<p>The earth's shape is an oblate spheroid.</p>
 	<div class="cta">
 		<button id="yes"><?php echo $yes ?></button>
 		<button id="no"><?php echo $no ?></button>
@@ -10,12 +10,12 @@
 <div class="face" id="answer">
 	<h2><?php echo $answer ?></h2>
 	<p>
-		<strong class="yes">Wrong.</strong>
-		<strong class="no">Correct!</strong> 
-		The sun is WAY bigger than the Earth. In fact the sun is 109 times bigger than the Earth, which means you'd have to line up 109 Earth's end-to-end to stretch across the face of the sun.
+		<strong class="no">Wrong.</strong>
+		<strong class="yes">Correct!</strong> 
+The Earth is neither flat like a pancake nor perfectly round like a marble. The actual shape of the Earth is actually an oblate spheroid, a sphere with a bulge around the equator. 
 	</p>
 	<div class="cta">
-		<a target="_blank" class="button" href="http://www.nasa.gov/vision/universe/solarsystem/sun_for_kids_main.html" id="more">I want to learn more about stars!</a>
+		<a target="_blank" class="button" href="http://www.universetoday.com/25893/is-the-earth-round/" id="more">I want to learn more about the earth!</a>
 		<a class="button" href="<?php echo $link_to_next ?>" id="next"><?php echo $next ?></a>
 	<div>
 </div>
