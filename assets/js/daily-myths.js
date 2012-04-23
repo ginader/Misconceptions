ff537a61c61a10b782ea108bf53f7536ae4d1ae5
Dirk Ginader <dirk@ginader.de>
2012-04-23 11:12:44 -0700

    hack fix for the chrome back face visible issue
    hiding the back face with js for chrome only (not something i like - if
    you have abetter suggestion PLEASE let me know)

--- assets/js/daily-myths.js
+++ assets/js/daily-myths.js
@@ -1,14 +1,30 @@
 $(document).ready(function() {
     $('#yes').click(function(){
         $('#answer .yes').show();
-        $('#answer .no').hide();
         $('#answer .galaxy').show();
-        $('#card').toggleClass('flipped');
     });
     $('#no').click(function(){
         $('#answer .no').show();
-        $('#answer .yes').hide();
-        $('#card').toggleClass('flipped');
+    });
+    // hack for the chrome backface visibility issue
+    var is_chrome = /chrome/.test( navigator.userAgent.toLowerCase() );
+    if(is_chrome){
+        $('#answer').addClass('hidden');
+    }
+    $('#no, #yes, #answer h2').click(function(){
+        if ($('#card.flipped').length > 0){
+            // show question
+            $('#card.flipped').removeClass('flipped').addClass('flipped_');
+            if(is_chrome){
+                setTimeout(function(){ $('#answer').addClass('hidden'); }, 500);// hack for the chrome backface visibility issue
+            }
+        }else{ 
+            // show answer
+            $('#card.flipped_').removeClass('flipped_').addClass('flipped');
+            if(is_chrome){
+                setTimeout(function(){ $('#answer').removeClass('hidden'); }, 500);// hack for the chrome backface visibility issue
+            }
+        }
     });
 
 
