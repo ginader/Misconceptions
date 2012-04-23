ee53678394eaa90a408a2690d9ce9a52b6960851
Dirk Ginader <dirk@ginader.de>
2012-04-23 09:39:05 -0700

    link on logo

--- de/about/index.php
+++ de/about/index.php
@@ -7,7 +7,7 @@ include ($_SERVER['DOCUMENT_ROOT'].'/head.inc.php');
 <div id="header">
 	<h1><a href="/">Daily Myths</a></h1>
 	<ul class="nav">
-		<li><a href="/en/about">about</a></li>
+		<li><a href="/de/about">about</a></li>
 		<!-- <li><a href="/credits">credits</a></li> -->
 		<li id="language-nav" class="open_">
 			<a href="/language">change language</a>
