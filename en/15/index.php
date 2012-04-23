5051bcbbf86a6d065911ad528fc5818c891a6952
Dirk Ginader <dirk@ginader.de>
2012-04-23 09:48:14 -0700

    fixed mixed up myth 15

--- en/15/index.php
+++ en/15/index.php
@@ -2,7 +2,7 @@
 <div class="face" id="question">
 	<h2><?php echo $question ?></h2>
 	<img src="/assets/img/img-sun.png" alt="A closeup of the Sun.">
-	<p>The Earth's revolution around the sun causes night and day.</p>
+	<p>The Sun goes around the Earth in less than a year</p>
 	<div class="cta">
 		<button id="yes"><?php echo $yes ?></button>
 		<button id="no"><?php echo $no ?></button>
@@ -14,7 +14,7 @@
 	<p>
 		<strong class="yes">Correct!</strong>
 		<strong class="no">Wrong.</strong> 
-		The Earth rotates on an axis, making one full rotation every 24 hours. Different sides of the Earth are exposed to the sun during the course of these 24 hours, with half of the planet being exposed to sun while the other half is deprived of light. It is this rotation that causes day and night.
+		The earth goes around the sun in 365.24 days. Most of the time our calendar is 365 days and every 4th year it is 366 days. That's why we have a leap year!
 	</p>
 	<div class="cta">
 		<a target="_blank" class="button" href="http://www.astronomy.org/astronomy/misconceptions.html#3" id="more">I want to learn more about the earth and sun!</a>
