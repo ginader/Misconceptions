c12daa2fec8b3743546b8f8a475bf2997c9f254c
Dirk Ginader <dirk@ginader.de>
2012-05-01 15:21:43 -0700

    new robust 3D flip that now nicely renders cross browser

--- assets/js/daily-myths.js
+++ assets/js/daily-myths.js
@@ -6,28 +6,18 @@ $(document).ready(function() {
         $('#answer .galaxy').show();
     });
     $('#no').click(function(){
+        $('#answer .galaxy').show();
         $('#answer .no').show();
         $('#answer .yes').hide();
     });
-    // hack for the chrome backface visibility issue
-    var is_chrome = /chrome/.test( navigator.userAgent.toLowerCase() );
-    if(is_chrome){
-        $('#answer').addClass('hidden');
-    }
-    $('#no, #yes, #answer h2').click(function(){
-        if ($('#card.flipped').length > 0){
-            // show question
-            $('#card.flipped').removeClass('flipped').addClass('flipped_');
-            if(is_chrome){
-                setTimeout(function(){ $('#answer').addClass('hidden'); }, 500);// hack for the chrome backface visibility issue
-            }
-        }else{ 
-            // show answer
-            $('#card.flipped_').removeClass('flipped_').addClass('flipped');
-            if(is_chrome){
-                setTimeout(function(){ $('#answer').removeClass('hidden'); }, 500);// hack for the chrome backface visibility issue
-            }
-        }
+
+    $('.block .cta button').click(function(){
+        $('.block').addClass('flip');
+    });
+    $('.block .back h2').click(function(e){
+        $('.block').removeClass('flip');
+
+        e.preventDefault();
     });
 
 
