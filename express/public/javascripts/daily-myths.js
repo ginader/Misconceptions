4b36df00f5881968c1af914558785c0e45e2e9bd
Dirk Ginader <dirk@ginader.de>
2012-05-14 16:06:43 -0700

    fix for the jumping page caused by scrollbars appearing on card flip

--- express/public/javascripts/daily-myths.js
+++ express/public/javascripts/daily-myths.js
@@ -1,7 +1,15 @@
+function clipPage(){    
+    if($(window).height() > $('body').height()){
+        $('body').addClass('clipped');
+    }else{
+        $('body').removeClass('clipped');
+    }
+}
 $(document).ready(function() {
     var is_chrome = /chrome/.test( navigator.userAgent.toLowerCase() ); //chrome is still misbehaving sometimes
     var can_historyPush = !!(window.history && history.pushState);
     var url = location.href;
+    url = url.substring(0,url.indexOf(window.languagecode)); // just the bare url looking like this: "http://dailymyths.org/""
     //console.log(url);
     var delimiter = (url.charAt(url.length-1) == '/' ) ? '' : '/';
     //console.log(delimiter);
@@ -10,7 +18,7 @@ $(document).ready(function() {
     $('#yes').click(function(e){
         e.preventDefault();
         if(can_historyPush){
-            history.pushState(null, null, url+delimiter+'yes/');
+            history.pushState(null, null, url+languagecode+'/'+id+'/yes/');
         }
         $('#answer .yes').show();
         $('#answer .no').hide();       
@@ -18,7 +26,7 @@ $(document).ready(function() {
     });
     $('#no').click(function(e){
         if(can_historyPush){
-            history.pushState(null, null, url+delimiter+'no/');
+            history.pushState(null, null, url+languagecode+'/'+id+'/no/');
         }
         e.preventDefault();
         $('#answer .no').show();
@@ -26,23 +34,26 @@ $(document).ready(function() {
     });
 
     if(can_historyPush){
-        $(window).bind( 'popstate', function(e){
-            if(location.href == url){
+        $(window).bind('popstate', function(e){
+            console.log(location.href);
+            if(location.href == url+languagecode+'/'+id+'/'){
                 // user used the back button - let's go back to the question then :)
                 $('.block').removeClass('flip');
                 //console.log('user used the back button');
-            }else if(location.href == url+delimiter+'yes/'){
+            }else if(location.href == url+languagecode+'/'+id+'/yes/'){
                 // user used the forward button
                 $('.block').addClass('flip');
                 $('#answer .yes').show();
                 $('#answer .no').hide();
                 //console.log('user used the forward button');
-            }else if(location.href == url+delimiter+'no/'){
+            }else if(location.href == url+languagecode+'/'+id+'/no/'){
                 // user used the forward button
                 $('.block').addClass('flip');
                 $('#answer .no').show();
                 $('#answer .yes').hide();
                 //console.log('user used the forward button');
+            }else{
+                //console.log('not sure');
             }
         });
     }
@@ -56,13 +67,13 @@ $(document).ready(function() {
                 $('#answer .galaxy').css('-webkit-perspective','none');
                 // removing perspective fixes the issue when Chrome does not show the solar system ani at all
             }
-        }, 500);
+        }, 600);
     });
 
 
     $('.block .back h2').click(function(e){
         if(can_historyPush){
-            history.pushState(null, null, url);
+            history.pushState(null, null, url+languagecode+'/'+id+'/');
         }
         $('.block').removeClass('flip');
         e.preventDefault();
@@ -83,6 +94,8 @@ $(document).ready(function() {
         $('html').addClass('unload');
     })
 
+    $(window).resize(clipPage);
+    clipPage();
 });
 $(window).load(function() {
     $('body').scrollTop(1);
