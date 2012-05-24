3149a37dae523c5335c452a1329db0943c54ec80
Dirk Ginader <dirk@ginader.de>
2012-05-24 11:31:57 -0700

    added support for dutch and vietnamese

--- express/app.js
+++ express/app.js
@@ -48,7 +48,7 @@ function getStrings(){
 };
 function getCards(){
 	// get cards
-	new YQL.exec("select * from csv where url='https://docs.google.com/spreadsheet/pub?key=0Ar3qBZOJQ4WBdDk3ajRQTzdKdGNTZnptR3lNYWZkMlE&single=true&gid=2&output=csv' and columns='id,active,truth,img_question,img_answer,question_en,answer_en,link,question_hi,answer_hi,question_ar,answer_ar,question_de,answer_de,question_es,answer_es,question_zh,answer_zh,question_fr,answer_fr'", function(response) {
+	new YQL.exec("select * from csv where url='https://docs.google.com/spreadsheet/pub?key=0Ar3qBZOJQ4WBdDk3ajRQTzdKdGNTZnptR3lNYWZkMlE&single=true&gid=2&output=csv' and columns='id,question_en,answer_en,active,truth,img_question,img_answer,link,question_hi,answer_hi,question_ar,answer_ar,question_de,answer_de,question_es,answer_es,question_zh,answer_zh,question_fr,answer_fr,question_nl,answer_nl,question_vi,answer_vi'", function(response) {
 		if (response.error) {
 			console.log("getCards Error: " + response.error.description);
 		} else {
